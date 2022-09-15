1c0c4bca263d2f7efc2f14e51209ec5b848b0a3a
Neel9672 <88261977+Neel9672@users.noreply.github.com>
2022-09-15 23:46:47 +0530

    NUMBER GUSSING GAME
    number gussing game with j

--- Main.java
+++ Main.java
@@ -1,10 +1,10 @@
-import
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         int attempt =1;
         int guess  =0;
-        int secret_number = (int) (math.random() * 99 +1);
-        scanner sc = new scanner(system.in);
+        int secret_number = (int) (Math.random() * 99 +1);
+        Scanner sc = new Scanner(System.in);
         System.out.println("Welcome to guess number game \n You will be asked  to guess a number to win the game \n You have 5 attempt limit");
 
         do{
@@ -31,6 +31,8 @@ public class Main {
             }
         }while( guess != secret_number);
 
-
+        System.out.println("the secret umber is ");
+        System.out.println(secret_number);
+          
     }
 }
